33207849d48c229f80f9cdf1ff2e9e09f83d70ef
JaskiratAnand <jaskiratanand01@gmail.com>
2024-11-26 00:41:29 +0530

    mocking return valus + adding giit workflows

--- express-with-vitest-prisma/src/index.ts
+++ express-with-vitest-prisma/src/index.ts
@@ -5,39 +5,42 @@ import { z } from "zod";
 export const app = express();
 app.use(express.json());
 
-const sumInput = z.object({
+const calculateInput = z.object({
     a: z.number(),
     b: z.number()
 })
 
-app.post("/sum", async (req: Request, res: any) => {
-    const parsedResponse = sumInput.safeParse(req.body)
-    
+app.post("/calculate", async (req: Request, res: any) => {
+    const parsedResponse = calculateInput.safeParse(req.body)
+
     if (!parsedResponse.success) {
         return res.status(411).json({
             message: "Incorrect inputs"
         })
     }
 
-    await prismaClient.sum.create({
+    const response = await prismaClient.calculate.create({
         data: {
             a: parsedResponse.data.a,
             b: parsedResponse.data.b,
             result: parsedResponse.data.a + parsedResponse.data.b,
+            type: "SUM"
         }
-    })
+    });
+
 
     res.json({
-        answer: parsedResponse.data.a + parsedResponse.data.b
+        answer: parsedResponse.data.a + parsedResponse.data.b,
+        id: response.id
     })
 });
 
-app.get("/sum", (req: Request, res: any) => {
-    const parsedResponse = sumInput.safeParse({
+app.get("/calculate", (req: Request, res: any) => {
+    const parsedResponse = calculateInput.safeParse({
         a: Number(req.headers["a"]),
         b: Number(req.headers["b"])
     })
-    
+
     if (!parsedResponse.success) {
         return res.status(411).json({
             message: "Incorrect inputs"
--- express-with-vitest-prisma/src/tests/index.test.ts
+++ express-with-vitest-prisma/src/tests/index.test.ts
@@ -1,27 +1,52 @@
 import { describe, expect, vi, it } from 'vitest';
 import request from "supertest";
 import { app } from "../index"
+import { prismaClient } from '../__mocks__/db';
 
 // mocking db request
 // vi.mock('../db', () => ({
-//   prismaClient: { sum: { create: vi.fn() } }
+//   prismaClient: { calculate: { create: vi.fn() } }
 // }));
 
 // deep mocking prisma client
-vi.mock('../db'); 
+vi.mock('../db');
 
-describe("POST /sum", () => {
-  it("should return the sum of two numbers", async () => {
-    const res = await request(app).post("/sum").send({
+describe("POST /calculate", () => {
+  it("should return the calculate of two numbers", async () => {
+
+    // mocking return values
+    prismaClient.calculate.create.mockResolvedValue({
+      id: 1,
+      a: 1,
+      b: 2,
+      type: "SUM",
+      result: 3
+    });
+
+    // spys
+    vi.spyOn(prismaClient.calculate, "create");
+
+    const res = await request(app).post("/calculate").send({
       a: 1,
       b: 2
     });
+
+    // verify spy
+    expect(prismaClient.calculate.create).toHaveBeenCalledWith({
+      data: {
+        a: 1,
+        b: 2,
+        type: "SUM",
+        result: 3
+      }
+    })
+    
     expect(res.statusCode).toBe(200);
     expect(res.body.answer).toBe(3);
   });
 
   it("should return 411 if no inputs are provided", async () => {
-    const res = await request(app).post("/sum").send({});
+    const res = await request(app).post("/calculate").send({});
     expect(res.statusCode).toBe(411);
     expect(res.body.message).toBe("Incorrect inputs");
   });
@@ -29,10 +54,10 @@ describe("POST /sum", () => {
 });
 
 
-describe("GET /sum", () => {
-  it("should return the sum of two numbers", async () => {
+describe("GET /calculate", () => {
+  it("should return the calculate of two numbers", async () => {
     const res = await request(app)
-      .get("/sum")
+      .get("/calculate")
       .set({
         a: "1",
         b: "2"
@@ -44,7 +69,7 @@ describe("GET /sum", () => {
 
   it("should return 411 if no inputs are provided", async () => {
     const res = await request(app)
-      .get("/sum").send();
+      .get("/calculate").send();
     expect(res.statusCode).toBe(411);
   });
 
